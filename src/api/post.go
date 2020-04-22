9822aba8de2d46514d12072137e6cd1b5c98443e
yoko <18836617@qq.com>
2020-04-22 20:15:20 +0800

    [MOD]增加tags参数

--- src/api/post.go
+++ src/api/post.go
@@ -4,52 +4,68 @@ import (
 	"blog-be/src/model"
 	"blog-be/src/rsp"
 	"blog-be/src/util"
-	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type SetPostParam struct {
+	Title    string   `json:"title"`
+	Content  string   `json:"content"`
+	Desc     string   `json:"desc"`
+	CoverUrl string   `json:"coverUrl"`
+	Tags     []string `json:"tags"`
+}
+
 func SetPost(c *gin.Context) {
-	var post model.Post
-	err := c.ShouldBind(&post)
-	if err != nil {
+	var postParam SetPostParam
+	if err := c.ShouldBind(&postParam); err != nil {
 		rsp.Failed(c, -1000, err.Error())
 		return
 	}
 
-	p := model.GetPostWithTitle(post.Title)
-	if p != nil {
+	// 不允许相同title的文章
+	if p := model.GetPostWithTitle(postParam.Title); p != nil {
 		rsp.Failed(c, -1002, "文章已存在")
 		return
 	}
 
-	if post.CoverUrl != "" {
-		imgUrl, err := util.UploadImg(post.CoverUrl)
+	// 将图片地址转换成七牛地址
+	if postParam.CoverUrl != "" {
+		imgUrl, err := util.UploadImg(postParam.CoverUrl)
 		if err != nil {
 			rsp.Failed(c, -1003, "上传CoverUrl失败")
 			return
 		}
-		post.CoverUrl = imgUrl
+		postParam.CoverUrl = imgUrl
 	}
 
-	_, err = post.Set()
-	if err != nil {
-		rsp.Failed(c, -1001, err.Error())
+	if err := savePost(&postParam); err != nil {
+		rsp.Failed(c, -1002, "上传文章失败")
 		return
 	}
 
 	rsp.Success(c, nil)
 }
 
+func savePost(param *SetPostParam) error {
+	p := model.Post{
+		Title:    param.Title,
+		Content:  []byte(param.Content),
+		Desc:     param.Desc,
+		CoverUrl: param.CoverUrl,
+	}
+	_, err := p.Set()
+	return err
+}
+
+// GetPostDescList 获取文章简介
 func GetPostDescList(c *gin.Context) {
 	posts, err := model.GetPostDescList()
 	if err != nil {
-		fmt.Println(err)
+		rsp.Failed(c, -1000, "获取文章简介失败")
+		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"data": gin.H{
-			"posts": posts,
-		},
+	rsp.Success(c, gin.H{
+		"posts": posts,
 	})
 }
--- src/model/postTag.go
+++ src/model/postTag.go
@@ -7,3 +7,13 @@ type PostTag struct {
 	CreateTime int `xorm:"created"`
 	UpdateTime int `xorm:"updated"`
 }
+
+func (p *PostTag) Set() (int64, error) {
+	n, err := db.Insert(&p)
+	return n, err
+}
+
+func (p *PostTag) Get() (bool, error) {
+	res, err := db.Get(p)
+	return res, err
+}
